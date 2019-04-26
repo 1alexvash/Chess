716ced31cd7bddc8b544cfe36db118c780c5a943
Alexander <ilordarkey@gmail.com>
2019-04-26 07:38:22 +0300

    Updates

---
+++ precache-manifest.c0bf4e8f5cd140df0124edd8dff2ba7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24462830402fe0069c9f",
+    "url": "/Chess/static/js/runtime~main.24462830.js"
+  },
+  {
+    "revision": "1893638daf4206c2450d",
+    "url": "/Chess/static/js/main.1893638d.chunk.js"
+  },
+  {
+    "revision": "bd6981d1c04700d45e96",
+    "url": "/Chess/static/js/2.bd6981d1.chunk.js"
+  },
+  {
+    "revision": "1893638daf4206c2450d",
+    "url": "/Chess/static/css/main.630e8156.chunk.css"
+  },
+  {
+    "revision": "4bcbc1bd1a559bee6dd262daa96368fd",
+    "url": "/Chess/index.html"
+  }
+];
